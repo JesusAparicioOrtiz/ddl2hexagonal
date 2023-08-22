cb3969f2e6e27d43e18f8e22cba32403ced7afbb
Jesus Aparicio Ortiz <japarior@mercadona.es>
2023-08-22 22:08:08 +0200

    feat: OneToMany relations and model imports support

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from models.column import Column
 from utils.conversions import Conversions
 
 def create_models(db_models_dir, table_definitions):
-    for table_name, columns, pks, mtofks in table_definitions:
-        table = Table(db_models_dir,table_name,columns,pks,mtofks)
-        table_camel_case_name = table.camel_case_name()
+    for table_name, columns, pks, mtofks, otmfks in table_definitions:
+        table = Table(db_models_dir, table_name, columns, pks, mtofks, otmfks)
+        table_camel_case_name = table.camel_case_table_name()
         if len(pks) > 1:
             if not os.path.exists(f'{db_models_dir}{table_camel_case_name}/pk'):
                 os.makedirs(f'{db_models_dir}{table_camel_case_name}/pk')
@@ -20,13 +20,13 @@ def create_models(db_models_dir, table_definitions):
 def parse_sql_schema(schema):
     statements = schema.split(';')
     table_definitions = []
-
+    otmfks = {}
     for statement in statements:
         statement = statement.lower()
         if "create table" in statement:
             lines = statement.strip().split('\n')
             table_name = lines[0].split()[2]
-            columns, pks, mtofks, otmfks = [], [], [], []
+            columns, pks, mtofks = [], [], []
             for line in lines[1:]:
                 if line.strip() == "(":
                     pass
@@ -41,11 +41,22 @@ def parse_sql_schema(schema):
                             else:
                                 if c.name in pks : c.compound_pk = True
                     elif "foreign key"in line:
-                        mtofks.append((line.split("references ")[1].split()[0],line.split("foreign key (")[1].split(")")[0].split(","),line.split("references ")[1].split("(")[1].split(")")[0].split(",")))
+                        referenced_table = line.split("references ")[1].split()[0]
+                        fk = line.split("foreign key (")[1].split(")")[0].split(",")
+                        referenced_column = line.split("references ")[1].split("(")[1].split(")")[0].split(",")
+                        mtofks.append((referenced_table,fk,referenced_column))
+                        if referenced_table not in otmfks:
+                            otmfks[referenced_table] = []
+                        if table_name not in otmfks[referenced_table]:
+                            otmfks[referenced_table].append(table_name)
                 elif not "constraint"in line:
                     lineParts = line.split()
                     columns.append(Column(lineParts[0].replace('"', ''),Conversions.convert_type(lineParts[1]),"not null" in line))
             table_definitions.append((table_name, columns, pks, mtofks))
+
+    for idx, (table_name, columns, pks, mtofks) in enumerate(table_definitions):
+        table_definition_with_otfmfks = (table_name, columns, pks, mtofks, otmfks.get(table_name, []))
+        table_definitions[idx] = table_definition_with_otfmfks
 
     return table_definitions
 
--- models/table.py
+++ models/table.py
@@ -3,7 +3,7 @@ from models.compound_key import Compound_key
 
 
 class Table:
-    def __init__(self, package, table_name, columns, pks, mtofks):
+    def __init__(self, package, table_name, columns, pks, mtofks, otmfks):
         self.package = package.split("src/main/java/")[1].replace("/",".")
         self.compound_key = None
         if len(pks) > 1:
@@ -13,9 +13,17 @@ class Table:
         self.columns = columns
         self.pks = pks
         self.mtofks = mtofks
+        self.otmfks = otmfks
 
-    def camel_case_name(self):
+    def camel_case_table_name(self):
         return Conversions.snake_to_camel(self.table_name)
+    
+    def add_models_imports(self):
+        model_imports = ""
+        for table in [mto[0] for mto in self.mtofks] + self.otmfks:
+            table_snake_case = Conversions.snake_to_camel(table)
+            model_imports += f"""import {self.package}{table_snake_case}.{table_snake_case.capitalize()}MO;\n"""
+        return model_imports
     
     def add_extra_imports(self):
         imports_dict = {
@@ -27,42 +35,55 @@ class Table:
         for c in self.columns:
             if c.data_type in imports_dict and imports_dict[c.data_type] not in imports:
                 imports += imports_dict.get(c.data_type)
+        if "List" not in imports and len(self.otmfks) > 0:
+            imports += "import java.util.List;\n"
         return imports
     
-    def join_column_definition(self,fk,table_referenced_column):
-        return f"""@JoinColumn(name = "{fk}",  referencedColumnName = "{table_referenced_column}", insertable = false, updatable = false)"""
+    def join_column_definition(self,fk,referenced_column):
+        return f"""@JoinColumn(name = "{fk}",  referencedColumnName = "{referenced_column}", insertable = false, updatable = false)"""
     
-    def add_fks(self):
-        fks_res = ""
+    def one_to_many_definition(self,referenced_table):
+        return f"""private List<{Conversions.snake_to_camel(referenced_table).capitalize()}MO> {Conversions.snake_to_camel(referenced_table)}MO;"""
+
+    def add_many_to_one_definitions(self):
+        res = ""
         many_to_one_tag = "@ManyToOne(fetch = FetchType.LAZY)"
         for t in self.mtofks:
             table_referenced = t[0]
             fks = t[1]
-            table_referenced_columns = t[2]
+            referenced_columns = t[2]
             attribute = f"""private {Conversions.snake_to_camel(table_referenced).capitalize()}MO {Conversions.snake_to_camel(table_referenced)}MO;\n\n"""
             if len(fks) > 1:
-                fks_res += f"""    {many_to_one_tag}\n    @JoinColumns(\u007b\n"""
+                res += f"""    {many_to_one_tag}\n    @JoinColumns(\u007b\n"""
                 for i in range(len(fks)):
-                    fks_res += "        " + (self.join_column_definition(fks[i],table_referenced_columns[i]))
+                    res += "        " + (self.join_column_definition(fks[i],referenced_columns[i]))
                     if i < len(fks) - 1:
-                        fks_res += ",\n"
-                fks_res += f"""    \n    \u007d)\n    {attribute}"""
+                        res += ",\n"
+                res += f"""    \n    \u007d)\n    {attribute}"""
             elif len(fks) == 1:
-                fks_res += f"""    {many_to_one_tag}\n    {self.join_column_definition(fks[0],table_referenced_columns[0])}\n    {attribute}"""
-        return fks_res
+                res += f"""    {many_to_one_tag}\n    {self.join_column_definition(fks[0],referenced_columns[0])}\n    {attribute}"""
+        return res
+    
+    def add_one_to_many_definitions(self):
+        res = ""
+        one_to_many_tag = f"""    @OneToMany(mappedBy = "{self.camel_case_table_name()}MO", fetch = FetchType.LAZY)"""
+        for t in self.otmfks:
+            res += f"""{one_to_many_tag}\n    {self.one_to_many_definition(t)}\n\n"""
+        return res
     
     def __str__(self):
         nl = "\n"
-        cpk = f"""\n    @EmbeddedId\n    private {self.camel_case_name().capitalize()}PK {self.camel_case_name()};\n""" if self.compound_key else ""
-        cpk_import = f"""import {self.package}{self.camel_case_name()}.pk.{self.camel_case_name().capitalize()}PK;""" if cpk else ""
-        return f"""package {self.package}{self.camel_case_name()};
+        cpk = f"""\n    @EmbeddedId\n    private {self.camel_case_table_name().capitalize()}PK {self.camel_case_table_name()};\n""" if self.compound_key else ""
+        cpk_import = f"""import {self.package}{self.camel_case_table_name()}.pk.{self.camel_case_table_name().capitalize()}PK;""" if cpk else ""
+        return f"""package {self.package}{self.camel_case_table_name()};
+
+{cpk_import}
+{self.add_models_imports()}
 
 import lombok.*;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 {self.add_extra_imports()}
-
-{cpk_import}
 
 @Entity
 @Table(name = "{self.table_name}")
@@ -71,10 +92,10 @@ import javax.validation.constraints.NotNull;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class {self.camel_case_name().capitalize()}MO \u007b
+public class {self.camel_case_table_name().capitalize()}MO \u007b
 {cpk}
 {nl.join([str(column) for column in self.columns])}
 
-{self.add_fks()}
+{self.add_many_to_one_definitions()}{self.add_one_to_many_definitions()}
 \u007d
 """
